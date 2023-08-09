b2d6db37d5bad5591c7836fc0cac54d67150bcca
toreyes0 <87428657+toreyes0@users.noreply.github.com>
2023-08-09 08:36:38 +0800

    update bio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@ import TypeAnimation from '../components/TypeAnimation.vue'
 		<p id="intro" class="text-lg delay-1000 animate-fadeIn">Hello! I'm</p>
 		<TypeAnimation id="name" class="text-6xl text-center my-2" />
 		<p id="desc" class="text-center sm:w-1/2 my-2 opacity-0 animate-fadeInDelayed">
-			4th year Computer Science student interested in web development and data analytics. Also
+			Computer Science graduate interested in web development and data analytics. Also
 			dabbled in machine learning and deep learning.
 		</p>
 		<div id="profiles" class="mt-4 opacity-0 animate-fadeInDelayedMore">
